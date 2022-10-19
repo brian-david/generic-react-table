6f45d07c10ada5ce7b87ece763cb6fb8dda7cff4
Brian Irons <the.brian.irons@gmail.com>
2022-10-19 10:54:15 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import "./styles.css";
+import Table from "./Table";
+
+const stockItems = [
+  {
+    id: 1,
+    name: "Pencil",
+    count: 3,
+    image: {
+      url:
+        "https://user-images.githubusercontent.com/14864439/101538058-52edaa00-397b-11eb-8107-ea606bf90929.png",
+      width: 100,
+      height: 50
+    }
+  },
+  {
+    id: 2,
+    name: "Paper",
+    count: 4,
+    image: {
+      url:
+        "https://user-images.githubusercontent.com/14864439/101538104-61d45c80-397b-11eb-8c56-b2de523b9aa3.png",
+      width: 100,
+      height: 50
+    }
+  },
+  {
+    id: 3,
+    name: "Scissors",
+    count: 4,
+    image: {
+      url:
+        "https://user-images.githubusercontent.com/14864439/101538129-68fb6a80-397b-11eb-8250-e622fdf0f34c.png",
+      width: 100,
+      height: 50
+    }
+  }
+];
+
+export default function App() {
+  return (
+    <div className="App">
+      <Table
+        headers={{
+          id: "Id",
+          name: "Name",
+          count: "Count",
+          image: "Image"
+        }}
+        items={stockItems}
+        customRenderers={{
+          image: (it) => (
+            <img
+              alt={`${it.name}`}
+              src={it.image.url}
+              width={it.image.width}
+              height={it.image.height}
+            />
+          )
+        }}
+      />
+    </div>
+  );
+}
